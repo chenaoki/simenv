8a1279c991bc40aee50b1d85732db58907cd4729
Naoki Tomii <chenaoki@gmail.com>
2016-03-09 15:32:56 +0900

    Initial commit

---
+++ setup/wget.sh
@@ -0,0 +1,12 @@
+#wget http://glaros.dtc.umn.edu/gkhome/fetch/sw/parmetis/parmetis-4.0.3.tar.gz
+#tar -zxvf parmetis-4.0.3.tar.gz
+#cd parmetis-4.0.3
+#make config shared=1
+#make
+#make install
+#cd metis
+#make
+#make install
+#cd ../../
+
+
